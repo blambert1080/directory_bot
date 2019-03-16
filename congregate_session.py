c50aaa6d36c1c71051bd4e18d7c297708988f0df
blambert1080 <blambert1080@gmail.com>
2019-03-15 23:27:41 -0500

    Remove comments

--- congregate_session.py
+++ congregate_session.py
@@ -46,39 +46,4 @@ def get_church_member_page_soup(family_link):
     return BeautifulSoup(church_member_page.content, 'html.parser')
 
 
-# def get_church_member_page(first_name, last_name):
-#     # Directory Searching
-#     directory_soup = get_directory_page_soup()
-#     if last_name and is_more_than_one_result(directory_soup, last_name):
-#         re_full_name = ("^(?=.*\\b"
-#                         + last_name
-#                         + "\\b)(?=.*\\b"
-#                         + first_name
-#                         + "\\b).*$")
-#         return get_member_number(directory_soup, re_full_name)
-#     else:
-#         return (get_all_member_numbers(directory_soup, first_name)
-#                 if is_more_than_one_result(directory_soup, first_name)
-#                 else get_member_number(directory_soup, first_name))
-
-
-# def is_more_than_one_result(soup, name):
-#     return len(soup.find_all('h3', string=re.compile(name))) > 1
-
-
-# def get_member_number(soup, name):
-#     return (None if not soup.find('h3', string=re.compile(name))
-#             else soup.find('h3', string=re.compile(name))
-#                      .find_parent('a')['href'])
-
-
-# def get_all_member_numbers(soup, name):
-#     names = {}
-#     index = 0
-#     for name in soup.find_all('h3', string=re.compile(name)):
-#         names[index] = get_member_number(soup, name.text.strip())
-#         index += 1
-#     return names
-
-
 login_congregate()
